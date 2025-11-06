72b1f53ecd15a958e50c29c14602f3789189f7a9
AlpoTurunen <Alpo.96@gmail.com>
2025-11-06 12:42:35 +0200

    update new values to settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,8 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="../config/.env", extra="ignore")
-    DW_URL: str
-    FILE_PATH: str
-    OUTPUT_PATH: str
-    CONVERTER_ENV: str
-    LOGGING: str
+    DW_URL: str = 'https://staging.laji.fi/laji-etl'
+    FILE_PATH: str = 'test_data/'
+    OUTPUT_PATH: str = ''
+    CONVERTER_ENV: str = 'staging'
+    LOGGING: str = 'DEBUG'
+    
+    # Email settings
+    SMTP_HOST: str = 'localhost'
+    SMTP_PORT: int = 25
+    SMTP_USER: str = 'user'
+    SMTP_PASSWORD: str = 'password'
+    SMTP_TLS: bool = True
+    FROM_EMAIL: str = 'geoconverter@luomus.fi'
+    ADMIN_EMAILS: str = 'firstname@lastname.fi'
+    EMAIL_ENABLED: bool = False
